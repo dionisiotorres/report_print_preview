887882c5e81a2d91a61d054b17dac3157944b5d4
kelvzxu <ceokelvin12@gmail.com>
2020-06-08 15:34:54 +0700

    create views and static directory

---
+++ static/src/js/user_menu.js
@@ -0,0 +1,32 @@
+// #################################################################################
+// # Author      : Kelvzxu (<https://kltech-intl.odoo.com/>)
+// # Copyright(c): 2015-kltech-intl.
+// # All Rights Reserved.
+// #
+// #
+// # This program is copyright property of the author mentioned above.
+// # You can`t redistribute it and/or modify it.
+// #################################################################################
+odoo.define("report_print_preview.report_menu", function (require) {
+    "use strict";
+
+    var UserMenu = require("web.UserMenu");
+    var session = require("web.session");
+
+    UserMenu.include({
+        _onMenuPreview: function() {
+            var self = this;
+            this.trigger_up("clear_uncommitted_changes", {
+                callback: function() {
+                    self._rpc({
+                        route: "/web/action/load", 
+                        params: { action_id: "report_print_preview.action_short_preview_print" }}).then(function(result) {
+                        result.res_id = session.uid;
+                        self.do_action(result);
+                    });
+                },
+            });
+        },
+    });
+
+});
